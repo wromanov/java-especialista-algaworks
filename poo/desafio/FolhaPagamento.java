6e3604bca558bd670a3235a60023a6b6a51a5a66
Walace Delgado <walacecastro@live.com>
2024-12-03 03:41:00 -0300

    atualização Poo

--- poo/desafio/FolhaPagamento.java
+++ poo/desafio/FolhaPagamento.java
@@ -9,13 +9,13 @@ public class FolhaPagamento {
         if (contrato.funcionario.temMaisDeUmFilho()) {
             double totalHoraNormal = quantidadeHorasNormaisTrabalhadas * contrato.valorHoraNormal;
             double totalHoraExtra = quantidadeHorasExtrasTrabalhadas * contrato.valorHoraExtra;
-            double totalPagar = (totalHoraNormal + totalHoraExtra) * 1.10;
-            return totalPagar;
+            double totalSalario = (totalHoraNormal + totalHoraExtra) * 1.10;
+            return totalSalario;
         } else {
             double totalHoraNormal = quantidadeHorasNormaisTrabalhadas * contrato.valorHoraNormal;
             double totalHoraExtra = quantidadeHorasExtrasTrabalhadas * contrato.valorHoraExtra;
-            double totalPagar = totalHoraNormal + totalHoraExtra;
-            return totalPagar;
+            double totalSalario = totalHoraNormal + totalHoraExtra;
+            return totalSalario;
         }
 
 
--- poo/desafio/Principal.java
+++ poo/desafio/Principal.java
@@ -3,6 +3,22 @@ package poo.desafio;
 public class Principal {
     public static void main(String[] args) {
 
+        Funcionario funcionario = new Funcionario();
+        funcionario.nome = "Walace";
+        funcionario.quantidadeFilhos = 4;
+
+        ContratoTrabalho contrato = new ContratoTrabalho();
+        contrato.funcionario = funcionario;
+        contrato.valorHoraNormal = 25.0;
+        contrato.valorHoraExtra = 35.0;
+
+        FolhaPagamento folhaPagamento = new FolhaPagamento();
+        folhaPagamento.quantidadeHorasExtrasTrabalhadas = 22;
+        folhaPagamento.quantidadeHorasNormaisTrabalhadas = 50;
+
+        System.out.print(folhaPagamento.calcularSalario(contrato));
+
+
 
     }
 }
