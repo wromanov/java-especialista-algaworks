a34e76fd1b1eadea0bd7284a818aa3011a7e9841
Walace Delgado <walacecastro@live.com>
2024-12-10 18:36:22 -0300

    Wrapper

---
+++ poo/wrapper/Cliente.java
@@ -0,0 +1,10 @@
+package poo.wrapper;
+
+public class Cliente {
+
+    String nome;
+
+    //Wrapper é uma classe que embrulha um tipo primitivo o transformando em um objeto.
+    Integer idade;
+    Double rendaMensal;
+}
